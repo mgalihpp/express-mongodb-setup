01ea03e9eb2febccd4694312c7d3385d5362e0d9
mgalihpp <muhammadgalih451@gmail.com>
2026-02-03 17:29:14 +0700

    Initial commit: Express.js with MongoDB setup

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// Connect to Database
+connectDB();
+
+// Middleware
+app.use(express.json());
+
+// Basic Route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
